f9b1206e9d1a64ef1f2eeb84902e42cc4432c5ee
Mauricio-Gonnet <122744079+Mauricio-Gonnet@users.noreply.github.com>
2023-05-28 01:28:05 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onPinPressed(TouchPin.P0, function () {
+    music.playTone(262, music.beat(BeatFraction.Half))
+})
+input.onPinPressed(TouchPin.P2, function () {
+    music.playTone(330, music.beat(BeatFraction.Half))
+})
+input.onPinPressed(TouchPin.P1, function () {
+    music.playTone(294, music.beat(BeatFraction.Half))
+})
